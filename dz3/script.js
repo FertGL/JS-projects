17cf713ff0a2a5fa46140eb571681e09d24b9207
gleb_fert <liant379@gmail.com>
2025-04-13 21:32:09 +0300

    dz 3 done

--- dz3/script.js
+++ dz3/script.js
@@ -18,3 +18,18 @@ for( let i = arr.length - 1; i >= 0; i++) {
 console.log(arr[arr.length-1]);
 arr[arr.length-1] = '';
 
+
+let arr2 = []; 
+
+for(let i = 1; 1 <= 10; i++) {
+    let a = prompt('Введите число');
+
+    if(+a % 2 == 0) {
+        arr2[arr2.length] = +a;
+    }
+}
+
+for( let i = arr.length - 1; i >= 0; i++) {
+    console.log(arr2[i]);
+}
+
