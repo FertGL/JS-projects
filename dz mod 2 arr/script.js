e22fcc0d11658fbed61f6a64f62cb88bce5d813a
gleb_fert <liant379@gmail.com>
2025-04-22 00:15:29 +0300

    dz mod 2 arr done

---
+++ dz mod 2 arr/script.js
@@ -0,0 +1,10 @@
+console.log(isUnique([1, 2, 3, 4]));
+console.log(isUnique([1, 2, 3, 2]));
+
+console.log(mergeArrays([1, 2, 3], [3, 4, 5]));
+
+console.log(replaceNegatives([-1, 2, -3, 4], 0));
+
+
+console.log(isPrime(7));
+console.log(getPrimes([1, 2, 3, 4, 5, 6, 7]));
